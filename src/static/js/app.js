4c4e5b97ef154799a79438e0f9b299b81f0924d1
lysiane-burel <77341053+lysiane-burel@users.noreply.github.com>
2023-01-11 12:29:30 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,8 +2,8 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
-            <h1>ToDo App by Lysiane Burel</h1>
             <Row>
+                <h1>ToDo App by Lysiane Burel</h1>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
                 </Col>
